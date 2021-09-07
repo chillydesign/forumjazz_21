2ad978f3206fbb4e63cabb5bcd089cf127d65ee2
Charles Harvey <harvey.charles@gmail.com>
2021-09-07 13:52:50 +0100

    add image to participants

--- sections/participants.php
+++ sections/participants.php
@@ -13,8 +13,14 @@
 
 
         <?php foreach ($participants as $participant) : ?>
+
+            <?php $image =  get_field('image',  "user_" . $participant->ID); ?>
             <div class="participant_container">
                 <h3><?php echo ($participant->display_name); ?></h3>
+
+                <?php if ($image) : ?>
+                    <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo ($participant->display_name); ?>" />
+                <?php endif; ?>
             </div>
 
         <?php endforeach ?>
