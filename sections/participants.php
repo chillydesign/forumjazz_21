302a373414863923a1b17d398268ba9a1dfb8471
Charles Harvey <harvey.charles@gmail.com>
2021-09-08 22:41:02 +0100

    nicer partiipants

--- sections/participants.php
+++ sections/participants.php
@@ -11,19 +11,27 @@
 
     <div class="container">
 
+        <div class="participant_containers">
 
-        <?php foreach ($participants as $participant) : ?>
 
-            <?php $image =  get_field('image',  "user_" . $participant->ID); ?>
-            <div class="participant_container">
-                <h3><?php echo ($participant->display_name); ?></h3>
+            <?php foreach ($participants as $participant) : ?>
 
-                <?php if ($image) : ?>
-                    <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo ($participant->display_name); ?>" />
-                <?php endif; ?>
-            </div>
+                <?php $image =  get_field('image',  "user_" . $participant->ID); ?>
+                <?php $structure =  get_field('structure_name',  "user_" . $participant->ID); ?>
+                <?php $position =  get_field('structure_position',  "user_" . $participant->ID); ?>
+                <div class="participant_container">
+                    <h3>
+                        <?php echo ($participant->first_name); ?>
 
-        <?php endforeach ?>
+                        <?php echo ($participant->last_name); ?>
+                    </h3>
+                    <p> <strong><?php echo $structure; ?></strong> &nbsp; <?php echo $position; ?></p>
+                    <?php if ($image) : ?>
+                        <!-- <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo ($participant->display_name); ?>" /> -->
+                    <?php endif; ?>
+                </div>
 
+            <?php endforeach ?>
+        </div>
     </div>
 </section>
