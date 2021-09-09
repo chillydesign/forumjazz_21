d0f32f4e1bf469f5775765e3dc981b44dfdbc864
Charles Harvey <harvey.charles@gmail.com>
2021-09-09 07:46:17 +0100

    add placeholder image to participants

--- sections/participants.php
+++ sections/participants.php
@@ -20,15 +20,20 @@
                 <?php $structure =  get_field('structure_name',  "user_" . $participant->ID); ?>
                 <?php $position =  get_field('structure_position',  "user_" . $participant->ID); ?>
                 <div class="participant_container">
-                    <h3>
-                        <?php echo ($participant->first_name); ?>
+                    <div class="participant_image">
+                        <?php if ($image) : ?>
+                            <!-- <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo ($participant->display_name); ?>" /> -->
+                        <?php endif; ?>
+                    </div>
+                    <div class="participant_text">
+                        <h3>
+                            <?php echo ($participant->first_name); ?>
+                            <?php echo ($participant->last_name); ?>
+                        </h3>
+                        <p> <strong><?php echo $structure; ?></strong> &nbsp; <?php echo $position; ?></p>
+                    </div>
 
-                        <?php echo ($participant->last_name); ?>
-                    </h3>
-                    <p> <strong><?php echo $structure; ?></strong> &nbsp; <?php echo $position; ?></p>
-                    <?php if ($image) : ?>
-                        <!-- <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo ($participant->display_name); ?>" /> -->
-                    <?php endif; ?>
+
                 </div>
 
             <?php endforeach ?>
