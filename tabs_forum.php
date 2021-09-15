2a0783403fc8cb74d7f392df9eac45bf92cab5ab
Charles Harvey <harvey.charles@gmail.com>
2021-09-15 19:36:18 +0100

    change forum link in tab

--- tabs_forum.php
+++ tabs_forum.php
@@ -1,7 +1,7 @@
 <?php $site_url = site_url(); ?>
 <form id="search_concerts_form">
     <div class="button_group">
-        <a href="<?php echo $site_url; ?>/forum" class="button"><?php _e('Programmation', 'blankslate'); ?></a>
+        <a href="<?php echo $site_url; ?>/rencontres" class="button"><?php _e('Programmation', 'blankslate'); ?></a>
         <a href="<?php echo $site_url; ?>/intervenants" class="button"><?php _e('Intervenants', 'blankslate'); ?></a>
         <a href="<?php echo $site_url; ?>/participants" class="button"><?php _e('Participants', 'blankslate'); ?></a>
     </div>
