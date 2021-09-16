f570dfed3e79a33c1fd4dcedc4d4c41ce0be9f38
Charles Harvey <harvey.charles@gmail.com>
2021-09-16 14:13:20 +0100

    ditto

--- event-shared.php
+++ event-shared.php
@@ -81,50 +81,51 @@
                                     <p> <strong><?php echo $structure; ?> </strong>
                                     </p>
                                 </div>
-                            <?php endforeach ?>
                             </div>
-
-                        <?php endif; ?>
-
-                        <?php if ($location) : ?>
-                            <?php $location_title = $location->post_title; ?>
-                            <?php $location_address = get_field('address', $location->ID); ?>
-                            <?php $lieu_json  = lieu_to_map_json($location); ?>
-                            <h3><?php _e('Lieu', 'blankslate'); ?>: <?php echo $location_title; ?></h3>
-                            <?php if ($lieu_json) : ?>
-                                <div id="map_container"></div>
-                                <script>
-                                    const map_locations = [
-
-                                        <?php echo json_encode($lieu_json); ?>
-                                    ];
-                                    const theme_directory = '<?php echo get_template_directory_uri(); ?>';
-                                </script>
-                            <?php endif; ?>
-                            <?php if ($location_address) : ?>
-                                <p><?php echo $location_address; ?></p>
-                            <?php endif; ?>
-                        <?php endif; ?>
-
-
+                        <?php endforeach ?>
                     </div>
-                    <div class="column">
 
-                        <?php if ($image) : ?>
-                            <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo get_the_title(); ?>" />
-                        <?php endif; ?>
+                <?php endif; ?>
 
-                        <?php if ($video_clip) :; ?>
-                            <?php $youtube_id = youtube_id_from_url($video_clip); ?>
-                            <iframe style="width: 100%" width="560" height="549" src="https://www.youtube.com/embed/<?php echo $youtube_id; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope" allowfullscreen></iframe>
-                        <?php endif; ?>
+                <?php if ($location) : ?>
+                    <?php $location_title = $location->post_title; ?>
+                    <?php $location_address = get_field('address', $location->ID); ?>
+                    <?php $lieu_json  = lieu_to_map_json($location); ?>
+                    <h3><?php _e('Lieu', 'blankslate'); ?>: <?php echo $location_title; ?></h3>
+                    <?php if ($lieu_json) : ?>
+                        <div id="map_container"></div>
+                        <script>
+                            const map_locations = [
 
-                        <?php if ($ticketing) : ?>
-                            <a class="button button_block button_large" href="<?php echo $ticketing; ?>"><?php _e('Tickets', 'blankslate'); ?></a>
-                        <?php endif; ?>
+                                <?php echo json_encode($lieu_json); ?>
+                            ];
+                            const theme_directory = '<?php echo get_template_directory_uri(); ?>';
+                        </script>
+                    <?php endif; ?>
+                    <?php if ($location_address) : ?>
+                        <p><?php echo $location_address; ?></p>
+                    <?php endif; ?>
+                <?php endif; ?>
 
 
-                    </div>
             </div>
+            <div class="column">
+
+                <?php if ($image) : ?>
+                    <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo get_the_title(); ?>" />
+                <?php endif; ?>
+
+                <?php if ($video_clip) :; ?>
+                    <?php $youtube_id = youtube_id_from_url($video_clip); ?>
+                    <iframe style="width: 100%" width="560" height="549" src="https://www.youtube.com/embed/<?php echo $youtube_id; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope" allowfullscreen></iframe>
+                <?php endif; ?>
+
+                <?php if ($ticketing) : ?>
+                    <a class="button button_block button_large" href="<?php echo $ticketing; ?>"><?php _e('Tickets', 'blankslate'); ?></a>
+                <?php endif; ?>
+
+
+            </div>
+        </div>
     </section>
 </article>
