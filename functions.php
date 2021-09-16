85bcd1a15bad7962c375d78789416fac1b73f88c
Charles Harvey <harvey.charles@gmail.com>
2021-09-16 18:31:19 +0100

    version bump

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ add_action('after_setup_theme', 'remove_json_api');
 
 
 function wf_version() {
-    return '0.1.3';
+    return '0.1.4';
 }
 
 add_action('wp_enqueue_scripts', 'blankslate_enqueue');
