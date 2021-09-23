353cb831e0b45d9475d8782536c371c63f178bbd
Charles Harvey <harvey.charles@gmail.com>
2021-09-23 12:29:48 +0100

    translate tab forum?

--- tabs_forum.php
+++ tabs_forum.php
@@ -1,12 +1,18 @@
 <?php $site_url = site_url(); ?>
 <form id="search_concerts_form">
     <div class="button_group">
-        <a href="<?php echo $site_url; ?>/rencontres?subpage=programme" class="button"><?php _e('Programme', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/rencontres?subpage=rencontres" class="button"><?php _e('Rencontres', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/rencontres?subpage=showcases" class="button"><?php _e('Showcases', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/extras-forum" class="button"><?php _e('Extras Forum', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/intervenants" class="button"><?php _e('Intervenants', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/participants" class="button"><?php _e('Participants', 'blankslate'); ?></a>
+
+
+        <?php get_permalink(get_page_by_path('rencontres')); ?>
+
+        <a href="<?php get_permalink(get_page_by_path('rencontres')); ?>?subpage=programme" class="button"><?php _e('Programme', 'blankslate'); ?></a>
+        <a href="<?php get_permalink(get_page_by_path('rencontres')); ?>?subpage=rencontres" class="button"><?php _e('Rencontres', 'blankslate'); ?></a>
+        <a href="<?php get_permalink(get_page_by_path('rencontres')); ?>?subpage=showcases" class="button"><?php _e('Showcases', 'blankslate'); ?></a>
+        <a href="<?php get_permalink(get_page_by_path('extras-forum')); ?>" class="button"><?php _e('Extras Forum', 'blankslate'); ?></a>
+        <a href="<?php get_permalink(get_page_by_path('intervenants')); ?>" class="button"><?php _e('Intervenants', 'blankslate'); ?></a>
+        <a href="<?php get_permalink(get_page_by_path('participants')); ?>" class="button"><?php _e('Participants', 'blankslate'); ?></a>
+
+
     </div>
     <?php if (!$args['hide_search']) : ?>
         <input type="text" id="search_concerts" placeholder="<?php _e('rechercher', 'blankslate'); ?> ..." />
