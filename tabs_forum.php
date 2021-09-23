2e967bc99b6a377473b55928bd65078895d13afe
Charles Harvey <harvey.charles@gmail.com>
2021-09-23 12:39:01 +0100

    ditto

--- tabs_forum.php
+++ tabs_forum.php
@@ -4,12 +4,12 @@
 
 
 
-        <a href="<?php get_permalink(322); ?>?subpage=programme" class="button"><?php _e('Programme', 'blankslate'); ?></a>
-        <a href="<?php get_permalink(322); ?>?subpage=rencontres" class="button"><?php _e('Rencontres', 'blankslate'); ?></a>
-        <a href="<?php get_permalink(322); ?>?subpage=showcases" class="button"><?php _e('Showcases', 'blankslate'); ?></a>
-        <a href="<?php get_permalink(705); ?>" class="button"><?php _e('Extras Forum', 'blankslate'); ?></a>
-        <a href="<?php get_permalink(331); ?>" class="button"><?php _e('Intervenants', 'blankslate'); ?></a>
-        <a href="<?php get_permalink(298); ?>" class="button"><?php _e('Participants', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(322); ?>?subpage=programme" class="button"><?php _e('Programme', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(322); ?>?subpage=rencontres" class="button"><?php _e('Rencontres', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(322); ?>?subpage=showcases" class="button"><?php _e('Showcases', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(705); ?>" class="button"><?php _e('Extras Forum', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(331); ?>" class="button"><?php _e('Intervenants', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(298); ?>" class="button"><?php _e('Participants', 'blankslate'); ?></a>
 
 
     </div>
