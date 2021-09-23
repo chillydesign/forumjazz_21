eeefb6b1ad7347072d72abb52b6c4a6703a5512c
Charles Harvey <harvey.charles@gmail.com>
2021-09-23 12:40:42 +0100

    ditto for tab concerts

--- tabs_concerts.php
+++ tabs_concerts.php
@@ -2,9 +2,13 @@
 
 <form id="search_concerts_form">
     <div class="button_group">
-        <a href="<?php echo $site_url; ?>/concerts" class="button"><?php _e('Programmation', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/jeune-public" class="button"><?php _e('Sélection jeune public', 'blankslate'); ?></a>
-        <a href="<?php echo $site_url; ?>/extras" class="button"><?php _e('Extras', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(320); ?>" class="button"><?php _e('Programmation', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(359); ?>" class="button"><?php _e('Sélection jeune public', 'blankslate'); ?></a>
+        <a href="<?php echo get_permalink(329); ?>" class="button"><?php _e('Extras', 'blankslate'); ?></a>
+
+
+
+
     </div>
     <input type="text" id="search_concerts" placeholder="<?php _e('rechercher', 'blankslate'); ?> ..." />
 </form>
