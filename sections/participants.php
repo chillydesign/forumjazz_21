5f0ec6b86d53796c7fd96d7cc3af52e70fd9dc89
Charles Harvey <harvey.charles@gmail.com>
2021-09-27 21:25:44 +0100

    ditto

--- sections/participants.php
+++ sections/participants.php
@@ -1,6 +1,6 @@
 <?php $participants = get_users(
     array(
-        'role' =>  array('participant', 'intervenant'),
+        'role' => 'participant,intervenant',
         'number' => -1,
         'orderby' => array('meta_value', 'login'),
         'meta_key' => 'last_name',
