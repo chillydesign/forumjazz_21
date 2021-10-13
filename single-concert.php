66583df5bf780a731ba1a459653790fb6268a651
Charles Harvey <harvey.charles@gmail.com>
2021-10-13 15:22:28 +0100

    different retour link for showcase concerts

--- single-concert.php
+++ single-concert.php
@@ -2,7 +2,16 @@
 <div class="container">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            <p class="breadcrumbs"><a href="<?php echo get_permalink(320); ?>?subpage=programme"><?php _e('retour', 'webfactor'); ?></a></p>
+            <p class="breadcrumbs">
+                <?php $for_forum = has_term('showcase', 'concert_category'); ?>
+                <?php if ($for_forum) : ?>
+                    <a href="<?php echo get_permalink(322); ?>?subpage=programme"><?php _e('retour', 'webfactor'); ?></a>
+                <?php else : ?>
+                    <a href="<?php echo get_permalink(320); ?>?subpage=programme"><?php _e('retour', 'webfactor'); ?></a>
+                <?php endif; ?>
+            </p>
+
+
             <?php get_template_part('event-shared'); ?>
 
     <?php endwhile;
