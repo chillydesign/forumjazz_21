c173c7c450009478334a006f358f548d71b487aa
Charles Harvey <harvey.charles@gmail.com>
2021-11-18 14:56:48 +0000

    add extraforum single

---
+++ single-extraforum.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div class="container">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <p class="breadcrumbs"><a href="<?php echo get_permalink(329); ?>"><?php _e('retour', 'webfactor'); ?></a></p>
+            <?php get_template_part('event-shared'); ?>
+
+    <?php endwhile;
+    endif; ?>
+
+</div>
+<?php get_footer(); ?>
